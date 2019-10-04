d35298245727996c124c537f428c7238d5370101
Tiago D. Teixeira <tiagoadmstz@yahoo.com.br>
2019-10-04 12:47:12 -0300

    04/10/2019

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://user:hNh4vZlBywf9jzsC@omnistack-f885s.mongodb.ne
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
